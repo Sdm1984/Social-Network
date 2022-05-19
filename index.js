dbcb4906376ceec96818e2bfad6cdd2ec246fb39
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-05-18 22:13:55 -0400

    add new folders

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 
+dotenv.config();
+
 
 app.listen(8800,()=>{
     console.log("Backend server test")
